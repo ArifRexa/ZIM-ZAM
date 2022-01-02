bc39304047f022f82c209f050c87cff95c89fd39
Arif reza <arif.reza3126@gmail.com>
2022-01-02 23:18:18 +0600

    initialization

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            here home starts
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            Here is navigation
+            
+        </div>
+    );
+};
+
+export default Header;
