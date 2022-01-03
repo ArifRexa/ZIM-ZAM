a86ad2a4c658925752aef0748e3d3cbbe3987ae4
alamin937 <mdalamin76812@gmail.com>
2022-01-03 10:20:03 +0600

    added

--- src/components/Home/Contemporary/Contemporary.js
+++ src/components/Home/Contemporary/Contemporary.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
 
 const Contemporary = () => {
     return (
         <div>
-            this is comtemporary
+           <Row>
+               <Col sm={12} md={6}>
+                   <div>
+                       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKL2vhPaoAAv-VhviRerTtDYCG_NnEvH0UYg&usqp=CAU" alt="" />
+                   </div>
+               </Col>
+               <Col sm={12} md={6}>
+                    <div style={{textAlign:'left'}}>
+                    <h1>Contemporary Lamp</h1>
+                    <p>Browse a large selection of contemporary table lamp options for sale, including tiffany-style lamps, touch lamps and bedside lamps for your bedroom</p>
+                    <Button>See More</Button>
+                    </div>
+               </Col>
+           </Row>
         </div>
     );
 };
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Contemporary from '../Contemporary/Contemporary';
 import Product from '../Product/Product';
+import Stylish from '../Stylish/Stylish';
 import Upcoming from '../Upcoming/Upcoming';
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
            <Banner></Banner>
            <Product></Product>
            <Upcoming></Upcoming>
+           <Stylish></Stylish>
+           <Contemporary></Contemporary>
         </div>
     );
 };
