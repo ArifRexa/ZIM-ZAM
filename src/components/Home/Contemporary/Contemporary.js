32c27bb3155c2c3bc41ec26063256c989d87d037
alamin937 <mdalamin76812@gmail.com>
2022-01-03 11:53:06 +0600

    added

--- src/components/Home/Contemporary/Contemporary.js
+++ src/components/Home/Contemporary/Contemporary.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
+import './Contemporary.css'
 
 const Contemporary = () => {
     return (
-        <div>
+        <div style={{width:'80%', margin:'0 auto', marginTop:'80px'}}>
            <Row>
                <Col sm={12} md={6}>
-                   <div>
-                       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKL2vhPaoAAv-VhviRerTtDYCG_NnEvH0UYg&usqp=CAU" alt="" />
+                   <div className='conte-image'>
+                       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6YAwha3v13jg1coiYUsBWKwEJdSwewkagIw&usqp=CAU" alt="" />
                    </div>
                </Col>
                <Col sm={12} md={6}>
-                    <div style={{textAlign:'left'}}>
+                    <div className="conte" style={{textAlign:'left'}}>
+                    <div>
                     <h1>Contemporary Lamp</h1>
                     <p>Browse a large selection of contemporary table lamp options for sale, including tiffany-style lamps, touch lamps and bedside lamps for your bedroom</p>
                     <Button>See More</Button>
+                    </div>
                     </div>
                </Col>
            </Row>
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Contemporary from '../Contemporary/Contemporary';
+import MethodSection from '../MethodSection/MethodSection';
 import Product from '../Product/Product';
 import Stylish from '../Stylish/Stylish';
+import StylishTwo from '../StylishTow/StylishTwo';
 import Upcoming from '../Upcoming/Upcoming';
 
 const Home = () => {
@@ -13,6 +15,8 @@ const Home = () => {
            <Upcoming></Upcoming>
            <Stylish></Stylish>
            <Contemporary></Contemporary>
+           <StylishTwo></StylishTwo>
+           <MethodSection></MethodSection>
         </div>
     );
 };
