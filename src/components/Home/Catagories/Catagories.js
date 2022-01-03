fd8a084a56bcd3c67ff77616d5033e6b0aef2967
alamin937 <mdalamin76812@gmail.com>
2022-01-03 14:00:38 +0600

    add

--- src/components/Home/Catagories/Catagories.js
+++ src/components/Home/Catagories/Catagories.js
@@ -4,7 +4,7 @@ import './Catagories.css'
 const Catagories = () => {
     return (
         <div style={{marginTop:'30px'}}>
-            <h1 style={{marginBottom:'20px', fontWeight:'700'}}>Product Catagories</h1>
+            <h1 style={{marginBottom:'20px', fontWeight:'700',color:'goldenrod'}}>Product Catagories</h1>
             <div className='cata' >
                 <div>
                     <img src='https://images.unsplash.com/photo-1625591340248-6d289000f96a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3VuZ2xhc3N8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='' />
