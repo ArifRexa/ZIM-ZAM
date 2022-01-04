2d4333379f15fd9603f3332ad54bb47cb6589b95
alamin937 <mdalamin76812@gmail.com>
2022-01-04 10:00:13 +0600

    add

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -3,8 +3,23 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            here is footer
-            
+           <div>
+               <div>
+                   <h4>About Us</h4>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+               </div>
+               <div>
+                   <h4>Usefull Links</h4>
+                   <p>Downloadable product</p>
+                   <p>On sale product</p>
+                   <p>Our new product</p>
+                   <p>Order tracking</p>
+               </div>
+               <div>
+                   <h4>Contact Us</h4>
+                   <span></span>
+               </div>
+           </div>
         </div>
     );
 };
